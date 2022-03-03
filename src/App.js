7ffab491d6b2d7d892f427d294810b062f4a5bc6
Abdulrachman Hasan <hasanabdulrachman@gmail.com>
2022-03-03 11:38:12 +0700

    Parse md using marked -  test drive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { marked } from 'marked'
+import { useState } from 'react'
 
 function App() {
+  const defRaw = `
+  # Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+  `
+
+  const [raw, setRaw] = useState(defRaw)
+  const [text, setText] = useState(marked.parse(defRaw))
+
+  const handleChange = (event) => {
+    setRaw(event.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea id="editor" value={raw} onChange={handleChange}></textarea>
+      <div id="preview">
+        {marked.parse(raw)}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
