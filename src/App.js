964ae948b18995438f5194b409a8aa911e7f23f1
Abdulrachman Hasan <hasanabdulrachman@gmail.com>
2022-03-03 15:01:39 +0700

    Create default text, chunking default in example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import './App.css'
 import ReactMarkdown from 'react-markdown'
 import remarkBreaks from 'remark-breaks'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import defaultText from './defaultText.txt'
 
 function App() {
-  const defRaw = `
+  const chunk1 = `
   # Welcome to my React Markdown Previewer!
 
   ## This is a sub-heading...
   ### And here's some other cool stuff:
+
+  `
+  const chunk2 = "Heres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\nif (firstLine == '```' && lastLine == '```') {\nreturn multiLineCode;\n}\n}\n```"
+  const chunk3 = `
+    
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+
+  There's also [links](https://www.freecodecamp.org), and
+  > Block Quotes!
+
+  And if you want to get really crazy, even tables:
+
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | -------------
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+
+  - And of course there are lists.
+    - Some are bulleted.
+      - With different indentation levels.
+          - That look like this.
+
+
+  1. And there are numbered lists too.
+  1. Use just 1s if you want!
+  1. And last but not least, let's not forget embedded images:
+
+  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
   `
 
-  const [raw, setRaw] = useState(defRaw)
+  const [raw, setRaw] = useState(chunk1+chunk2+chunk3)
 
   const handleChange = (event) => {
     setRaw(event.target.value)
